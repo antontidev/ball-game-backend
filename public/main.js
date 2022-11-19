06e85eb0785fed93bfd56c4ea7a9213e44ccb80a
Anton Sukharev <sanjaplushka@gmail.com>
2022-11-19 13:53:41 +0500

    Testing routes

--- public/main.js
+++ public/main.js
@@ -1,13 +1,7 @@
 function run() {
-  fetch("/api/movie")
+  fetch("/leaderboard")
     .then((response) => response.json())
     .then((data) => {
-      const detailsElement = document.getElementById("movie");
-
-      detailsElement.getElementsByTagName("img")[0].src = data.poster;
-      detailsElement.getElementsByTagName("h1")[0].innerText = data.title;
-      detailsElement.getElementsByTagName("p")[0].innerText = data.fullplot;
-
-      detailsElement.style.visibility = "visible";
+      console.log(data)
     });
 }
