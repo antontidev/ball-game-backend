3548189c8396ce7a991c2f53257e530a0c1d073f
Anton Sukharev <sanjaplushka@gmail.com>
2022-11-19 15:20:37 +0500

    Add cors policy

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ const uri = process.env.MONGODB_URI || "mongodb-url";
 // use the express-static middleware
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
